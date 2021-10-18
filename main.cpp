e42b872bc31b45bbf3e5ee732f510b7211b2e9f7
ILYA655 <75269869+ILYA655@users.noreply.github.com>
2021-10-18 22:19:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <iostream>
 #include <locale.h>
@@ -8,9 +6,9 @@
 #include <stack> // стек
 using namespace std;
 
-typedef struct List{
+typedef struct List {
 	int Data;
-	List* next;
+	struct List* next;
 }List;
 
 List* init(int num)
@@ -23,9 +21,10 @@ List* init(int num)
 
 List* add(List* lst, int num)
 {
-	struct List *temp, *p;
+	struct List* temp, * p;
 	temp = (List*)malloc(sizeof(List));
 	p = lst->next;
+	lst->next = temp;
 	temp->Data = num;
 	temp->next = p;
 	return temp;
@@ -43,9 +42,9 @@ void initialize(int n, List** vrt)
 void list(int n, int i, int** A1, List* lst)
 {
 	int j;
-	for (j = 0; j < n; j++)
+	for (j = n; j >= 0; j--)
 	{
-		if(A1[i][j] == 1);
+		if (A1[i][j] == 1)
 			add(lst, j);
 	}
 }
@@ -70,7 +69,7 @@ void print(List** vrt, int n)
 		{
 			if (vrt[i] != NULL)
 			{
-				cout << vrt[i]->Data + 1 << " ";
+				cout << vrt[i]->Data<< " ";
 				vrt[i] = vrt[i]->next;
 			}
 		}
@@ -81,12 +80,13 @@ void print(List** vrt, int n)
 
 void DFS_list(bool* A2, int n, int i, List** vrt)
 {
+	int j;
 	A2[i] = true;
 	cout << i << " ";
 	while (vrt[i]->next != NULL)
 	{
 		vrt[i] = vrt[i]->next;
-		int j = vrt[i]->Data;
+		j = vrt[i]->Data;
 		if (A2[j] == false)
 			DFS_list(A2, n, j, vrt);
 	}
@@ -96,32 +96,32 @@ void DFS_list(bool* A2, int n, int i, List** vrt)
 void steck(int** A1, int n, int i)
 {
 	stack<int> Stack;
-  	int *nodes; // вершины графа
+	int* nodes; // вершины графа
 	nodes = (int*)malloc(n * sizeof(int*));
-  	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 		nodes[i] = 0; // исходно все вершины равны 0
 	int a;
-	cout << "Enter vertex: " ;
+	cout << "Enter vertex: ";
 	cin >> a;
-	Stack.push(a); // помещаем в очередь первую вершину
+	Stack.push(a); // помещаем в очередь вершину
 	cout << "DFS stack result: ";
 	while (!Stack.empty()) // пока стек не пуст
 	{
 		int node = Stack.top(); // извлекаем вершину
-    	Stack.pop();
-    	if (nodes[node] == 2) continue;
-    	nodes[node] = 2; // отмечаем ее как посещенную
-    	for (int j = n - 1; j >= 0; j--) // проверяем для нее все смежные вершины
-    	//for (int j = 0; j < n; j++)
+		Stack.pop();
+		if (nodes[node] == 2) continue;
+		nodes[node] = 2; // отмечаем ее как посещенную
+		for (int j = n - 1; j >= 0; j--) // проверяем для нее все смежные вершины
+		//for (int j = 0; j < n; j++)
 		{
-      		if (A1[node][j] == 1 && nodes[j] != 2) // если вершина смежная и не обнаружена
-      		{
-	    	    Stack.push(j); // добавляем ее в cтек
-    		    nodes[j] = 1; // отмечаем вершину как обнаруженную
-      		}
-    	}
-	
-    cout << node << " "; // выводим номер вершины
+			if (A1[node][j] == 1 && nodes[j] != 2) // если вершина смежная и не обнаружена
+			{
+				Stack.push(j); // добавляем ее в cтек
+				nodes[j] = 1; // отмечаем вершину как обнаруженную
+			}
+		}
+
+		cout << node << " "; // выводим номер вершины
 	}
 	cout << "\n";
 	cin.get();
@@ -132,7 +132,7 @@ int** create(int n)
 	int** A1, k = 1;
 	A1 = (int**)malloc(n * sizeof(int*));
 	srand(time(NULL));
-	for(int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		A1[i] = (int*)malloc(n * sizeof(int));
 		A1[i][i] = 0;
@@ -141,14 +141,15 @@ int** create(int n)
 			if (rand() % 100 > 50)
 			{
 				A1[i][j] = 0;
-			} else{
+			}
+			else {
 				A1[i][j] = 1;
 			}
 		}
 		k++;
 	}
 	k = 1;
-	for(int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		for (int j = k; j < n; j++)
 		{
@@ -160,7 +161,7 @@ int** create(int n)
 	for (int i = 0; i < n; i++)
 		cout << "V" << i << " ";
 
-	for(int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		cout << "\n";
 		for (int j = 0; j < n; j++)
@@ -187,9 +188,10 @@ void DFS(bool* A2, int n, int i, int** A1)
 
 int main()
 {
-	int n, **t, s, o;
+	int n, ** t, s, o;
 	cout << "Enter array size: ";
 	cin >> n;
+	List** vrt = (List**)malloc(n * sizeof(List*));
 	bool* A2 = (bool*)malloc(n * sizeof(bool));
 	t = create(n);
 	cout << "\n";
@@ -201,12 +203,8 @@ int main()
 	DFS(A2, n, s, t);
 	cout << "\n";
 	steck(t, n, 0);
-	
-	List** vrt = (List**)malloc(n * sizeof(List*));
-	
 	initialize(n, vrt);
 	result(vrt, t, n);
-	
 	print(vrt, n);
 	cout << "enter vertex: ";
 	cin >> o;
@@ -214,10 +212,11 @@ int main()
 	for (int i = 0; i < n; i++)
 		A2[i] = false;
 	DFS_list(A2, n, o, vrt);
-	system("pause");
+	cout << "\n";
 	for (int i = 0; i < n; i++)
 		free(t[i]);
 	free(t);
 	free(A2);
+	//system("pause");
 	return 0;
 }
